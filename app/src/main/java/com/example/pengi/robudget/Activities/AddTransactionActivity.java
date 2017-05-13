c419475f6bc4e6adc34d1c811e3827e0f2f50198
Andrew <chenzinee@live.com>
2017-05-13 17:31:07 +1000

    Fixed Add Transaction and View Transaction Classes (from Add/View Entities)

--- app/src/main/java/com/example/pengi/robudget/Activities/AddTransactionActivity.java
+++ app/src/main/java/com/example/pengi/robudget/Activities/AddTransactionActivity.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import com.example.pengi.robudget.R;
-
 /**
  * Created by Pengi on 11/05/2017.
  */
--- app/src/main/java/com/example/pengi/robudget/Activities/ViewTransactionActivity.java
+++ app/src/main/java/com/example/pengi/robudget/Activities/ViewTransactionActivity.java
@@ -1,8 +1,20 @@
 package com.example.pengi.robudget.Activities;
 
-/**
- * Created by Pengi on 13/05/2017.
- */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class ViewTransactionActivity {
-}
+    public class ViewTransactionActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_view_transactions);
+        }
+
+        public void sendMessage(View view) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
+    }
