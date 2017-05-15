056976fa46577dccf61fa521220211505d6c1b6d
Andrew <chenzinee@live.com>
2017-05-15 20:46:54 +1000

    Sub-menus for account options (Bank and Card detials and Password change)

--- app/src/main/java/com/example/pengi/robudget/Activities/AccountActivity.java
+++ app/src/main/java/com/example/pengi/robudget/Activities/AccountActivity.java
@@ -1,7 +1,13 @@
 package com.example.pengi.robudget.Activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 import com.example.pengi.robudget.R;
 
@@ -14,5 +20,6 @@ public class AccountActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        }
     }
-}
---
+++ app/src/main/java/com/example/pengi/robudget/Activities/AccountBankCardActivity.java
@@ -0,0 +1,19 @@
+package com.example.pengi.robudget.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.pengi.robudget.R;
+
+/**
+ * Created by chenz on 15/05/2017.
+ */
+//TODO link sub menus to main activity
+
+public class AccountBankCardActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_bankcarddetails);
+    }
+}
